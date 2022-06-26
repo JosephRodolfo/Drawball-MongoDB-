90d1bd5696f180c70ab17d30a9098ed7e5c27918
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-06-26 18:50:48 -0400

    finish first rough version of real time updating of pixel placement from other players with socket.io

--- src/index.js
+++ src/index.js
@@ -1,108 +1,112 @@
-const {app} = require('./app')
-const port = process.env.PORT
-const {addUser, removeUser, getUsersInRoom, switchRooms }= require('./utils/users')
-
-
+const { Socket } = require("socket.io");
+const { app } = require("./app");
+const port = process.env.PORT;
+const {
+  addUser,
+  removeUser,
+  getUser,
+  switchRooms,
+} = require("./utils/users");
 
 const server = app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
 
-
-
 const io = require("socket.io")(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   },
 });
 
-
-
-
 io.on("connection", (socket) => {
-
-
-
-  socket.on('join', (options, callback) => {
-    const { error, user } = addUser({ id: socket.id, ...options })
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
 
     if (error) {
-        return callback(error)
+      return callback(error);
     }
 
-    socket.join(user.room)
+    socket.join(user.room);
 
-    socket.emit('message', generateMessage('Welcome!'))
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.id} has joined!`))
+    socket.emit("message", generateMessage("Welcome!"));
 
-    callback()
-})
+    callback();
+  });
 
+  socket.on("sendUpdate", (update, callback) => {
+    const { user, error } = getUser(socket.id);
 
-socket.on("sendUpdate", (update, callback) => {
-  socket.emit('message', generateMessage(`has painted this pixel done ${update}`))
-
-  callback("Delivered!");
-
-
-
-});
-
-
-
-// socket.on('switch', ({id, room}) => {
-//   console.log(id, room)
-//    const user = switchRooms(id, room)
-//   console.log(user, 'has switched rooms');
-//   if (user) {
-//       io.to(user.room).emit('message', generateMessage(`${user.id} has switched rooms!`))
-
-//   }
-// })
-
-
-
-socket.on('switch', (options, callback) => {
-  const { error, user } = switchRooms({ id: socket.id, ...options })
-
-  if (error) {
-      return callback(error)
-  }
-
-
-  socket.emit('message', generateMessage('user swithced rooms!'))
-  socket.broadcast.to(user.room).emit('message', generateMessage(`${user.id} has joined!`))
-
-  callback()
-})
-
-
-  socket.on('leave', (id) => {
-    const user = removeUser(id)
-    console.log(user + " has left");
-    if (user) {
-        io.to(user.room).emit('message', generateMessage(`${user.id} has left!`))
-
+    if (error || !user) {
+      return callback(error);
     }
-  }
+
+    socket.emit(
+      "message",
+      generateMessage(`${user.id} is you, and you have painted in ${user.room}`)
+    );
+
+    socket.to(user.room)
+      .emit(
+        "message",
+        generateMessage(`${user.id} has updated the board in room ${user.room}!`)
+      );
+
+      socket.broadcast
+      .to(user.room)
+      .emit(
+        "transferCoords",
+        generateUpdate(update)
+
+
+      )
+
+  });
+
+
+  socket.on("switch", (options, callback) => {
+
+    const { error, room, user } = switchRooms({ id: socket.id, ...options });
+    if (error || !room) {
+      return callback(error);
+
     
-  )
+    }
+
+    socket.leave(user.room)
+
+      socket.join(room);
+
+    user.room = room;
+    socket.emit("message", generateMessage( `user swithced rooms from ${user.room}`));
+    socket.to(user.room)
+      .emit("message", generateMessage(`${user.userId} has joined!`));
+
+    callback();
+  });
+
+  socket.on("leave", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.leave(user.room)
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.userId} has left!`)
+      );
+    }
+  });
 
 
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.leave(user.room)
 
-
-
-
-
-  // console.log('client connected')
-  // let connectCounter = io.engine.clientsCount;
-  // socket.emit("message", generateMessage(connectCounter));
-  // socket.broadcast.emit("message", generateMessage(connectCounter));
-  // socket.on("disconnect", () => {
-  //   let connectCounter = io.engine.clientsCount;
-  //   socket.emit("message", generateMessage(connectCounter));
-  //   socket.broadcast.emit("message", generateMessage(connectCounter));
-  // });
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.userId} has left!`)
+      );
+    }
+  });
 });
 
 const generateMessage = (text) => {
@@ -110,3 +114,8 @@ const generateMessage = (text) => {
     text,
   };
 };
+const generateUpdate = (coords) => {
+  return {
+    coords
+  };
+}
--- src/utils/users.js
+++ src/utils/users.js
@@ -2,31 +2,30 @@ const users = [];
 
 //addUser, removeUser, getUser, getUsers
 
-const addUser = ({ id, room }) => {
-
+const addUser = ({ id, userId, room }) => {
 
   //validate data
-  if (!id || !room) {
+  if (!userId || !room) {
     return {
-      error: "Username and room are required!",
+      error: "UserId and room are required!",
     };
   }
 
   //check for existing user
   const existingUser = users.find((user) => {
-    return user.room === room && user.id === id;
+    return user.room === room && user.userId === userId;
   });
 
   //validate username
   if (existingUser) {
-    return {
-      error: "id is in use!",
-    };
+    let newId= existingUser.id
+    removeUser(newId)
   }
+ 
 
-  const user = { id, room };
+
+  const user = { id, userId, room };
   users.push(user);
-
   return { user };
 };
 
@@ -35,29 +34,35 @@ const removeUser = (id) => {
 
   if (index !== -1) {
 
-    return users.splice(index, 1);
+   const user = users.splice(index, 1);
+   return user;
   }
+
 };
 
 const getUser = (id) => {
-    return users.findIndex((user) => user.id === id);
+      const index=   users.findIndex((user) => user.id === id);
+         return {user: users[index], index: index}
 };
 
 const getUsersInRoom = (room) => {
   return users.filter((user) => user.room === room);
 };
 
-const switchRooms = ({id, room})=>{
-   let index = getUser(id);
-    users[index].room = room;
+const switchRooms = ({id, userId, room})=>{
+   const indexObject = getUser(id);
+
+   const index = indexObject.index
+
 
     if (index ===-1) {
         return {
-          error: "no user with that id!",
+          error: "no room!",
         };
       }
-    const user = users[index];
-    return {user}
+      // users[index].room = room;
+      const user = users[index];
+    return { user, room }
 
 }
 
