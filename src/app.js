6b7e330b36d0ff26879e637b3a4dc781b9b80134
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-07-11 10:53:44 -0400

    prepare for heroku deployment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const app = express();
+require("dotenv").config();
 app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({limit: '50mb', extended: true}));
