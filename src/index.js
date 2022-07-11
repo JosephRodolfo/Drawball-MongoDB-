2effb2bf5a245427b81d25afc582b29f195229b5
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-07-11 12:04:28 -0400

    prepare for heroku deployment

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const io = require("socket.io")(server, {
   handlePreflightRequest: (req, res) => {
     const headers = {
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": process.env.DOMAIN, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Origin": 'https://josephrodolfo.github.io', //or the specific origin you want to give access to,
         "Access-Control-Allow-Credentials": true
     };
     res.writeHead(200, headers);
