2d8334bd5072d0e61b5a37ddef95050949115119
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-07-11 11:33:11 -0400

    prepare for heroku deployment

--- src/index.js
+++ src/index.js
@@ -13,127 +13,127 @@ const server = app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
 
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: process.env.DOMAIN,
-//     credentials: true,
-//   },
-// });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.DOMAIN,
+    credentials: true,
+  },
+});
 
-// io.on("connection", (socket) => {
-//   socket.on("join", (options, callback) => {
-//     const { error, user } = addUser({ id: socket.id, ...options });
+io.on("connection", (socket) => {
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
 
-//     if (error) {
-//       return callback(error);
-//     }
+    if (error) {
+      return callback(error);
+    }
 
-//     socket.join(user.room);
+    socket.join(user.room);
 
-//     socket.emit("message", generateMessage("Welcome!"));
+    socket.emit("message", generateMessage("Welcome!"));
 
-//     getUsersInRoom(user.room).length > 1
-//       ? io.in(user.room).emit("startSharePosition", true)
-//       : io.in(user.room).emit("startSharePosition", false);
+    getUsersInRoom(user.room).length > 1
+      ? io.in(user.room).emit("startSharePosition", true)
+      : io.in(user.room).emit("startSharePosition", false);
 
-//     callback();
-//   });
+    callback();
+  });
 
-//   socket.on("sendUpdate", (update, callback) => {
-//     const { user, error } = getUser(socket.id);
+  socket.on("sendUpdate", (update, callback) => {
+    const { user, error } = getUser(socket.id);
 
-//     if (error || !user) {
-//       return callback(error);
-//     }
+    if (error || !user) {
+      return callback(error);
+    }
 
-//     socket.emit(
-//       "message",
-//       generateMessage(`${user.id} is you, and you have painted in ${user.room}`)
-//     );
+    socket.emit(
+      "message",
+      generateMessage(`${user.id} is you, and you have painted in ${user.room}`)
+    );
 
-//     socket
-//       .to(user.room)
-//       .emit(
-//         "message",
-//         generateMessage(
-//           `${user.id} has updated the board in room ${user.room}!`
-//         )
-//       );
+    socket
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage(
+          `${user.id} has updated the board in room ${user.room}!`
+        )
+      );
 
-//     socket.broadcast
-//       .to(user.room)
-//       .emit("transferCoords", generateUpdate(update));
-//   });
+    socket.broadcast
+      .to(user.room)
+      .emit("transferCoords", generateUpdate(update));
+  });
 
-//   socket.on("switch", (options, callback) => {
-//     const { error, room, user } = switchRooms({ id: socket.id, ...options });
+  socket.on("switch", (options, callback) => {
+    const { error, room, user } = switchRooms({ id: socket.id, ...options });
 
-//     if (error || !room) {
-//       return callback(error);
-//     }
+    if (error || !room) {
+      return callback(error);
+    }
 
-//     socket.leave(user.room);
+    socket.leave(user.room);
 
-//     socket.join(room);
+    socket.join(room);
 
-//     user.room = room;
+    user.room = room;
 
-//     getUsersInRoom(room).length > 1
-//       ? io.in(room).emit("startSharePosition", true)
-//       : io.in(room).emit("startSharePosition", false);
+    getUsersInRoom(room).length > 1
+      ? io.in(room).emit("startSharePosition", true)
+      : io.in(room).emit("startSharePosition", false);
 
-//     socket.emit(
-//       "message",
-//       generateMessage(`user swithced rooms from ${user.room}`)
-//     );
-//     socket
-//       .to(user.room)
-//       .emit("message", generateMessage(`${user.userId} has joined!`));
+    socket.emit(
+      "message",
+      generateMessage(`user swithced rooms from ${user.room}`)
+    );
+    socket
+      .to(user.room)
+      .emit("message", generateMessage(`${user.userId} has joined!`));
 
-//     callback();
-//   });
+    callback();
+  });
 
-//   socket.on("leave", () => {
-//     const user = removeUser(socket.id);
-//     if (user) {
-//       socket.leave(user.room);
-//       io.to(user.room).emit(
-//         "message",
-//         generateMessage(`${user.userId} has left!`)
-//       );
-//     }
-//   });
+  socket.on("leave", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.leave(user.room);
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.userId} has left!`)
+      );
+    }
+  });
 
-//   socket.on("disconnect", () => {
-//     const user = removeUser(socket.id);
-//     if (user) {
-//       socket.leave(user.room);
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      socket.leave(user.room);
 
-//       io.to(user.room).emit(
-//         "message",
-//         generateMessage(`${user.userId} has left!`)
-//       );
-//     }
-//   });
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.userId} has left!`)
+      );
+    }
+  });
 
-//   socket.on("incrementInk", async (options) => {
+  socket.on("incrementInk", async (options) => {
 
-//     const {id, ink} = options;
-//     const ship = await Ship.testFindById(id);
-//     ship.inkLevel = ink;
-//     ship.save()
-
-
-//   });
-// });
+    const {id, ink} = options;
+    const ship = await Ship.testFindById(id);
+    ship.inkLevel = ink;
+    ship.save()
 
 
+  });
+});
 
-// const generateMessage = (text) => {
-//   return {
-//     text,
-//   };
-// };
-// const generateUpdate = (coords) => {
-//   return coords;
-// };
+
+
+const generateMessage = (text) => {
+  return {
+    text,
+  };
+};
+const generateUpdate = (coords) => {
+  return coords;
+};
